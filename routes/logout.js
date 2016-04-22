fb6cfb27a6159bb76602c6a4d80787efe46a66c9
liadbiz <zhuhaihao_hust@firefox.com>
2016-04-22 21:49:05 +0800

    add feature logi, signup, and logout

---
+++ routes/logout.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    req.session.user = null;
+    res.redirect('/');
+});
+
+module.exports = router;
